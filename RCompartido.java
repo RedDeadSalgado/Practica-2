a739d97845a09aa2d9a22fbf6f3f03b7a3599c36
RedDeadSalgado <55566701+RedDeadSalgado@users.noreply.github.com>
2020-09-07 16:59:43 -0500

    Add files via upload

---
+++ RCompartido.java
@@ -0,0 +1,20 @@
+package condicionescompetencias;
+
+/**
+ *
+ * @author btepozromero
+ */
+public class RCompartido {
+        private String  datoCompartido;
+        RCompartido(){
+                datoCompartido = "";
+        }
+        public String getDatoCompartido() {
+                return datoCompartido;
+        }
+
+        public void setDatoCompartido(String datoCompartido) {
+                this.datoCompartido = datoCompartido;
+        }
+       
+}
